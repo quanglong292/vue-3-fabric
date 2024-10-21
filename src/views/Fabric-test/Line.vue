a53b10541b5aa8897551631ad91d801a87e906be
quanglong292 <quang.long292@gmail.com>
2024-10-22 00:26:45 +0700

    Constraint Drawing

--- src/views/Fabric-test/Line.vue
+++ src/views/Fabric-test/Line.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <button class="border-2" @click="toggleDrawingLineMode">
+    <button @click="toggleDrawingLineMode">
       {{ isDrawingLine ? "Disable" : "Enable" }} Line Drawing Mode
     </button>
-    <canvas ref="canvasContainer" style="border: 1px solid #ccc"></canvas>
+    <canvas ref="canvasElement" style="border: 1px solid #ccc"></canvas>
   </div>
 </template>
 
@@ -15,18 +15,16 @@ export default {
   name: "FabricLineExample",
   setup() {
     const canvas = ref(null);
-    const canvasContainer = ref(null);
+    const canvasElement = ref(null);
     const isDrawingLine = ref(false);
     let line, isDown;
 
     onMounted(() => {
       // Initialize the Fabric.js canvas
-      canvas.value = new fabric.Canvas(canvasContainer.value, {
+      canvas.value = new fabric.Canvas(canvasElement.value, {
         width: 800,
         height: 600,
       });
-
-      canvas.value.selection = false;
 
       // Event listeners for drawing a line
       canvas.value.on("mouse:down", (options) => {
@@ -43,19 +41,29 @@ export default {
       canvas.value.on("mouse:move", (options) => {
         if (!isDrawingLine.value || !isDown) return;
         const pointer = canvas.value.getPointer(options.e);
-        line.set({ x2: pointer.x, y2: pointer.y });
+        const origX = line.x1;
+        const origY = line.y1;
+        let x2 = pointer.x;
+        let y2 = pointer.y;
+
+        // If shift key is pressed, make the line straight (horizontal, vertical, or 45 degrees)
+        if (options.e.shiftKey) {
+          const dx = Math.abs(x2 - origX);
+          const dy = Math.abs(y2 - origY);
+          if (dx > dy) {
+            y2 = origY; // Make line horizontal
+          } else {
+            x2 = origX; // Make line vertical
+          }
+        }
+
+        line.set({ x2, y2 });
         canvas.value.renderAll();
       });
 
       canvas.value.on("mouse:up", () => {
-        if (
-          line &&
-          Math.abs(line.x1 - line.x2) < 1 &&
-          Math.abs(line.y1 - line.y2) < 1
-        ) {
-          canvas.value.remove(line);
-        }
         isDown = false;
+        // Log current canvas data to ensure it doesn't store unnecessary data
         const canvasData = canvas.value.toJSON();
         console.log("Current Canvas Data:", canvasData);
       });
@@ -64,10 +72,11 @@ export default {
     // Toggle drawing line mode
     const toggleDrawingLineMode = () => {
       isDrawingLine.value = !isDrawingLine.value;
+      canvas.value.selection = !isDrawingLine.value;
     };
 
     return {
-      canvasContainer,
+      canvasElement,
       isDrawingLine,
       toggleDrawingLineMode,
     };
