92b333dac97e907ae77a2c3debb029df246daa6e
quanglong292 <quang.long292@gmail.com>
2024-10-21 23:34:43 +0700

    foundation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <main>
-    <RouterView />
+    <!-- <RouterView /> -->
+    <!-- <Pencil /> -->
+    <Line />
   </main>
 </template>
 
 <script setup lang="ts">
 import { useRoute, RouterView, RouterLink } from "vue-router";
+import Pencil from "./views/Fabric-test/Pencil.vue";
+import Line from "./views/Fabric-test/Line.vue";
 
 const route = useRoute();
 </script>
